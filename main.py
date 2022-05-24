cb094e93d20cbfe0216ec3d6e4e7f9a8d6e7f9a3
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-24 18:06:13 +0200

    add: the bot is now able to connect to Telegram

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder
+import asyncio
+from telegram.bot import Bot
+from os import getenv
+
+def init_telegram_connection() -> Bot:
+    token = getenv("QDBotToken")
+    app = Bot(token)
+    return app
+
+async def main() -> None:
+    app = init_telegram_connection()
+
+if __name__ == '__main__':
+    asyncio.run(main())
