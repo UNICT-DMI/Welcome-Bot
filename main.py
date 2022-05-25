929210e88f68c0d90765c270c69fc20d8dbbf035
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-25 15:52:32 +0200

    add: static welcome message

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from os import getenv
-from telegram.ext import ApplicationBuilder, MessageHandler, filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, CallbackContext, filters
+
+def get_new_user_name(user: dict) -> str:
+    return user['username'] if user['username'] != '' else user['first_name']
+
+
+async def send_welcome(update: Update, ctx: CallbackContext) -> None:
+    for new_member in update['message']['new_chat_members']:
+        new_member_username = get_new_user_name(new_member)
+        print(new_member['language_code'])
+        # TODO: Implement array for random welcome messages
+        await update.message.reply_text(f'Ciao {new_member_username} ^-^')
 
 def main() -> None:
     token = getenv("QDBotToken")
     app = ApplicationBuilder().token(token).build()
-    app.add_handler(MessageHandler(filters.ChatType.GROUPS & (filters.StatusUpdate.NEW_CHAT_MEMBERS), None))
+    app.add_handler(MessageHandler(
+        filters.ChatType.GROUPS & filters.StatusUpdate.NEW_CHAT_MEMBERS, 
+        send_welcome
+    ))
     app.run_polling()
 
 
