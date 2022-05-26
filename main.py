12d8b0dce3ddd9d5683e1cd131141e2207ab8b75
Valeri0p <24795760+Valeri0p@users.noreply.github.com>
2022-05-26 17:19:08 +0200

    fix: bot selfnotifing

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from os import getenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, CallbackContext, filters
 
-
 def get_new_user_name(user: dict) -> str:
     return f"@{user['username']}" if user['username'] != '' else user['first_name']
 
@@ -12,7 +11,9 @@ async def send_welcome(update: Update, ctx: CallbackContext) -> None:
         new_member_username = get_new_user_name(new_member)
         print(new_member['language_code'])
         # TODO: Implement array for random welcome messages
-        await update.message.reply_text(f'Ciao {new_member_username} ^-^')
+
+        if new_member_username != "@QDTelegramBot":
+            await update.message.reply_text(f'Ciao {new_member_username} ^-^')
 
 def main() -> None:
     token = getenv("QDBotToken")
