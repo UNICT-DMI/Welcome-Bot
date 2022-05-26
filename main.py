a1073c0c6e87ad9e53a7c1d5ff54d408499c6ac2
Valeri0p <24795760+Valeri0p@users.noreply.github.com>
2022-05-26 19:21:37 +0200

    check if new member is a bot

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def get_new_user_name(user: dict) -> str:
 
 async def send_welcome(update: Update, ctx: CallbackContext) -> None:
     for new_member in update['message']['new_chat_members']:
-        new_member_username = get_new_user_name(new_member)
-        print(new_member['language_code'])
-        # TODO: Implement array for random welcome messages
+        if not new_member['is_bot']:
+            new_member_username = get_new_user_name(new_member)
+            print(new_member['language_code'])
+            # TODO: Implement array for random welcome messages
 
-        if new_member_username != "@QDTelegramBot":
             await update.message.reply_text(f'Ciao {new_member_username} ^-^')
 
 def main() -> None:
