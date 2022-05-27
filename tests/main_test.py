206afd72974664642cfe9f58dd9d621c1e177c30
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-27 20:19:20 +0200

    improvement: users test are now parametrized

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,7 +1,15 @@
+import asyncio
 import pytest
+import pytest_asyncio
 from pytest_mock import MockerFixture
 import src.main as main
 
-def test_Users() -> None:
-    assert main.get_new_user_name(main.User(id=0, first_name='user', is_bot=True, username='user')) == '@user'
-    assert main.get_new_user_name(main.User(id=0, first_name='user', is_bot=True)) == 'user'
+users_list = [
+    {'func': main.get_new_user_name, 'expected_res': '@user', 'arg': main.User(id=0, first_name='user', is_bot=True, username='user')},
+    {'func': main.get_new_user_name, 'expected_res': 'user', 'arg': main.User(id=0, first_name='user', is_bot=True)}
+]
+
+@pytest.mark.parametrize('test', users_list)
+def test_Users(test: dict) -> None:
+    res = test['func'](test['arg'])
+    assert res == test['expected_res']
