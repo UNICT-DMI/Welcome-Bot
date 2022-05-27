150a0d72aeeabe142d2eaf1391ef16fc4ed3a943
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-27 18:26:35 +0200

    feat: implements first unit test

---
+++ tests/main_test.py
@@ -0,0 +1,8 @@
+from imghdr import tests
+import pytest
+from pytest_mock import MockerFixture
+import src.main as main
+
+def test_Users() -> None:
+    assert main.get_new_user_name(main.User(id=0, first_name='user', is_bot=True, username='user')) == '@user'
+    assert main.get_new_user_name(main.User(id=0, first_name='user', is_bot=True)) == 'user'
