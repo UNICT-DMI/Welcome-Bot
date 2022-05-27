0e29c02c30928fdd85643af12da6b51f85807944
Valeri0p <24795760+Valeri0p@users.noreply.github.com>
2022-05-27 16:08:12 +0200

    refactoring and moving the code to another method

--- main.py
+++ main.py
@@ -9,27 +9,27 @@ def get_new_user_name(user: dict) -> str:
     #fallback does not work properly
     return f"@{user['username']}" if user['username'] != '' else user['first_name']
 
+def generate_welcome(new_member) -> str:
+
+    new_member_username = get_new_user_name(new_member)
+
+    print(new_member['language_code'])
+    #print(type(new_member['language_code']))
+
+    # TODO: refactor, more entries in welcome.json
+    wlc_txt = ""
+    with open("welcome.json", "r") as f:
+        list = json.load(f)[new_member["language_code"] if type(new_member["language_code"]) == type("una stringa") else "en"]
+        wlc_txt = list[random.randrange(0,2)].replace("USER",new_member_username)
+
+    print(wlc_txt)
+    return wlc_txt
+
 
 async def send_welcome(update: Update, ctx: CallbackContext) -> None:
     for new_member in update['message']['new_chat_members']:
         if not new_member['is_bot']:
-            new_member_username = get_new_user_name(new_member)
-
-            print(new_member['language_code'])
-            #print(type(new_member['language_code']))
-
-            # TODO: move this to a different method, refactor, more entries in welcome.json
-            wlc_txt = ""
-            wlc_num = str(random.randrange(1,2))
-            #print(wlc_num)
-            with open("welcome.json", "r") as f:
-                list = json.load(f)[new_member["language_code"] + wlc_num if type(new_member["language_code"]) == type("una stringa") else "en" + wlc_num]
-                wlc_txt = list[0]
-                wlc_txt = wlc_txt.replace("USER",new_member_username)
-
-            print(wlc_txt)
-
-            await update.message.reply_text(f'{wlc_txt}')
+            await update.message.reply_text(f'{generate_welcome(new_member)}')
 
 def main() -> None:
     token = getenv("QDBotToken")
