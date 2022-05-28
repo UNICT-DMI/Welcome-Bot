0dfda86cd7e3bfbe6790b284548998abc78feadc
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-28 11:57:20 +0200

    feat: implemented first two unit tests with a generic function

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,15 +1,40 @@
-import asyncio
 import pytest
-import pytest_asyncio
 from pytest_mock import MockerFixture
+from telegram import Message, Chat
+from datetime import datetime
 import src.main as main
 
-users_list = [
-    {'func': main.get_new_user_name, 'expected_res': '@user', 'arg': main.User(id=0, first_name='user', is_bot=True, username='user')},
-    {'func': main.get_new_user_name, 'expected_res': 'user', 'arg': main.User(id=0, first_name='user', is_bot=True)}
+possible_users = [
+    main.User(id=0, first_name='user', is_bot=True, username='user'),  # bot with username
+    main.User(id=0, first_name='user', is_bot=True),                   # bot without username
+    main.User(id=0, first_name='user', is_bot=False, username='user'), # user with username
+    main.User(id=0, first_name='user', is_bot=False)                   # user without username
 ]
 
-@pytest.mark.parametrize('test', users_list)
-def test_Users(test: dict) -> None:
-    res = test['func'](test['arg'])
+tests = [
+    {'func': main.get_new_user_name, 'expected_res': '@user', 'arg': (possible_users[0],), 'is_async': False},
+    {'func': main.get_new_user_name, 'expected_res': 'user',  'arg': (possible_users[1],), 'is_async': False},
+    {'func': main.get_new_user_name, 'expected_res': '@user', 'arg': (possible_users[2],), 'is_async': False},
+    {'func': main.get_new_user_name, 'expected_res': 'user',  'arg': (possible_users[3],), 'is_async': False},
+    { 
+        'func': main.send_welcome, 
+        'expected_res': None, 
+        'arg': (main.Update(0, message=Message(0, new_chat_members=possible_users, chat=Chat(0, type='GROUP'), date=datetime.now())), None),
+        'mock_obj': Message,
+        'mock_func': 'reply_text',
+        'mock_ret': True,
+        'is_async': True
+    }
+]
+
+@pytest.mark.parametrize('test', tests)
+async def test_generic(mocker: MockerFixture, test: dict) -> None:
+    if test.get('mock_obj') is not None:
+        mocker.patch.object(test['mock_obj'], test['mock_func'], return_value=test['mock_ret'])
+    
+    if test['is_async']:
+        res = await test['func'](*test['arg'])
+    else:
+        res = test['func'](*test['arg'])
+
     assert res == test['expected_res']
