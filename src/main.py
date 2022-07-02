eab2a39bb9a74e8b93a27531c5e7ba8fba38da53
Domenico Blanco <doddo97@me.com>
2022-07-02 17:17:05 +0200

    fix: change languages order

--- src/main.py
+++ src/main.py
@@ -13,10 +13,10 @@ def generate_welcome(new_member: User) -> str:
     new_member_username = get_new_user_name(new_member)
 
     match new_member["language_code"]:
-        case "it" | _ :
-            lan_code = "it"
         case "en":
             lan_code = "en"
+        case "it" | _ :
+            lan_code = "it"
 
     with open("src/welcome.json", "r") as f:
         wlc_mess_list = load(f)[lan_code]
